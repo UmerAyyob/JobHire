876849add2092c156dc847758a4a222c7df95ec6
Umer Ayyub <newn09127@gmail.com>
2024-08-17 13:25:29 +0500

    changes for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html",
+  content: [
+    "./public/index.html",
     "./public/findjobs.html",
     "./public/employers.html",
     "./public/pricing.html",
@@ -13,36 +14,34 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundColor:{
-        btnBgYellowish:"rgb(253 224 71) ",
-        btnBgGreenish:"rgb(232,242,241)",
-        jobTypesBg:"#D9EBE9",
+      backgroundColor: {
+        btnBgYellowish: "rgb(253 224 71) ",
+        btnBgGreenish: "rgb(232,242,241)",
+        jobTypesBg: "#D9EBE9",
       },
-      colors:{
-        btntextGreenish:"rgb(34,19,94)",
+      colors: {
+        btntextGreenish: "rgb(34,19,94)",
       },
-      textUnderlineOffset:{
-        'negative-5':'-5px',
-        'negative-10':'-10px',
-        'negative-20':'-20px',
-      }
+      textUnderlineOffset: {
+        "negative-5": "-5px",
+        "negative-10": "-10px",
+        "negative-20": "-20px",
+      },
     },
-    
   },
   plugins: [
-    function({addUtilities}){
+    function ({ addUtilities }) {
       addUtilities({
-        '.underline-negative-offset-5':{
-          textUnderlineOffset:'-5px'
+        ".underline-negative-offset-5": {
+          textUnderlineOffset: "-5px",
         },
-        '.underline-negative-offset-10':{
-          textUnderlineOffset:'-10px'
-        },'.underline-negative-offset-15':{
-          textUnderlineOffset:'-15px'
+        ".underline-negative-offset-10": {
+          textUnderlineOffset: "-10px",
         },
-      },
-    )
-    }
+        ".underline-negative-offset-15": {
+          textUnderlineOffset: "-15px",
+        },
+      });
+    },
   ],
-}
-
+};
